cea636ba11f76069ab0b71193d39d66fd9bb73c2
gabriel-dev <policias@test.com>
2023-06-18 19:21:50 +0000

    task: implement roles module

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace GestionVehiculos.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace GestionVehiculos.Models;
 
 public class User
 {
-
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
 }
